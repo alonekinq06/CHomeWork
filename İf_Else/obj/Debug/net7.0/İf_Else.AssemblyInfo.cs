6d7b58cebe4eec5e0e414110ac8fd2f2d1ba3929
Furkan <Alonekinq06@gmail.com>
2023-01-09 18:36:30 +0300

    ifelse1

--- İf_Else/obj/Debug/net7.0/İf_Else.AssemblyInfo.cs
+++ İf_Else/obj/Debug/net7.0/İf_Else.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("İf_Else_if_if_Ternary_if")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("İf_Else")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("İf_Else_if_if_Ternary_if")]
-[assembly: System.Reflection.AssemblyTitleAttribute("İf_Else_if_if_Ternary_if")]
+[assembly: System.Reflection.AssemblyProductAttribute("İf_Else")]
+[assembly: System.Reflection.AssemblyTitleAttribute("İf_Else")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
